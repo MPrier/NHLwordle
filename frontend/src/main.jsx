eab1d5e924efe6a6047009869f275d2c2b050d6c
Matthew Prier <59553462+MPrier@users.noreply.github.com>
2025-11-10 13:34:48 -0500

    remove unused pages, dependency updates

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import image from './img/image.png';
-import GamePage from './pages/gamePage.jsx'
-
-let playerInfo = {image: image, name:'Wayne Fartzky', careerPoint: 2587};
+import StaticApp from './StaticPage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        {/* Route for the Home Page */}
-        <Route path="/" element={<GamePage/>} />
-      </Routes>
-    </Router>
+    <StaticApp />
   </StrictMode>,
 
 )
